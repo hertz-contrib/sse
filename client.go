e5d95a652ae7e7db46d7a46ac7e7dcaa0d533163
violapioggia <2604296771@qq.com>
2023-10-20 14:13:46 +0800

    feat:adapt hertz client

--- client.go
+++ client.go
@@ -1,124 +1,404 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 package sse
 
 import (
-	"bufio"
+	"bytes"
 	"context"
+	"encoding/base64"
+	"encoding/binary"
 	"errors"
 	"fmt"
-	hertz "github.com/cloudwego/hertz/pkg/app/client"
+	"github.com/cloudwego/hertz/pkg/app/client"
 	"github.com/cloudwego/hertz/pkg/protocol"
-	"log"
+	"io"
 	"net/http"
-	"strings"
+	"sync"
+	"sync/atomic"
 	"time"
+
+	"gopkg.in/cenkalti/backoff.v1"
 )
 
-//	type Client struct {
-//		Retry             time.Time
-//		ReconnectStrategy backoff.BackOff
-//		disconnectcb      ConnCallback
-//		connectedcb       ConnCallback
-//		subscribed        map[chan *Event]chan struct{}
-//		Headers           map[string]string
-//		ReconnectNotify   backoff.Notify
-//		ResponseValidator ResponseValidator
-//		Connection        *http.Client
-//		URL               string
-//		LastEventID       atomic.Value // []byte
-//		maxBufferSize     int
-//		mu                sync.Mutex
-//		EncodingBase64    bool
-//		Connected         bool
-//	}
-type Client struct {
-	hertzClient *hertz.Client
+var (
+	headerID    = []byte("id:")
+	headerData  = []byte("data:")
+	headerEvent = []byte("event:")
+	headerRetry = []byte("retry:")
+)
+
+func ClientMaxBufferSize(s int) func(c *Client) {
+	return func(c *Client) {
+		c.maxBufferSize = s
+	}
 }
 
-type EventStreamReader struct {
-	scanner *bufio.Scanner
+// ConnCallback defines a function to be called on a particular connection event
+type ConnCallback func(c *Client)
+
+// ResponseValidator validates a response
+type ResponseValidator func(c *Client, resp *protocol.Response) error
+
+// Client handles an incoming server stream
+type Client struct {
+	Retry             time.Time
+	ReconnectStrategy backoff.BackOff
+	disconnectcb      ConnCallback
+	connectedcb       ConnCallback
+	subscribed        map[chan *Event]chan struct{}
+	Headers           map[string]string
+	ReconnectNotify   backoff.Notify
+	ResponseValidator ResponseValidator
+	HertzClient       *client.Client
+	URL               string
+	LastEventID       atomic.Value // []byte
+	maxBufferSize     int
+	mu                sync.Mutex
+	EncodingBase64    bool
+	Connected         bool
 }
 
 // NewClient creates a new client
-func NewClient() *Client {
-	client := hertz.Client{}
+func NewClient(url string, opts ...func(c *Client)) *Client {
+	c := &Client{
+		URL:           url,
+		HertzClient:   &client.Client{},
+		Headers:       make(map[string]string),
+		subscribed:    make(map[chan *Event]chan struct{}),
+		maxBufferSize: 1 << 16,
+	}
 
-	return &Client{hertzClient: &client}
+	for _, opt := range opts {
+		opt(c)
+	}
+
+	return c
 }
 
-func (c *Client) Subscribe(ctx context.Context, req *protocol.Request) error {
-	// 设置 SSE 请求头
+// Subscribe to a data stream
+func (c *Client) Subscribe(stream string, handler func(msg *Event)) error {
+	return c.SubscribeWithContext(context.Background(), stream, handler)
+}
+
+// SubscribeWithContext to a data stream with context
+func (c *Client) SubscribeWithContext(ctx context.Context, stream string, handler func(msg *Event)) error {
+	operation := func() error {
+		resp, err := c.request(ctx, stream)
+		if err != nil {
+			return err
+		}
+		if validator := c.ResponseValidator; validator != nil {
+			err = validator(c, resp)
+			if err != nil {
+				return err
+			}
+		} else if resp.StatusCode() != 200 {
+			resp.CloseBodyStream()
+			return fmt.Errorf("could not connect to stream: %s", http.StatusText(resp.StatusCode()))
+		}
+		defer resp.CloseBodyStream()
+
+		reader := NewEventStreamReader(resp.BodyStream(), c.maxBufferSize)
+		eventChan, errorChan := c.startReadLoop(reader)
+
+		for {
+			select {
+			case err = <-errorChan:
+				return err
+			case msg := <-eventChan:
+				handler(msg)
+			}
+		}
+	}
+
+	// Apply user specified reconnection strategy or default to standard NewExponentialBackOff() reconnection method
+	var err error
+	if c.ReconnectStrategy != nil {
+		err = backoff.RetryNotify(operation, c.ReconnectStrategy, c.ReconnectNotify)
+	} else {
+		err = backoff.RetryNotify(operation, backoff.NewExponentialBackOff(), c.ReconnectNotify)
+	}
+	return err
+}
+
+// SubscribeChan sends all events to the provided channel
+func (c *Client) SubscribeChan(stream string, ch chan *Event) error {
+	return c.SubscribeChanWithContext(context.Background(), stream, ch)
+}
+
+// SubscribeChanWithContext sends all events to the provided channel with context
+func (c *Client) SubscribeChanWithContext(ctx context.Context, stream string, ch chan *Event) error {
+	var connected bool
+	errch := make(chan error)
+	c.mu.Lock()
+	c.subscribed[ch] = make(chan struct{})
+	c.mu.Unlock()
+
+	operation := func() error {
+		resp, err := c.request(ctx, stream)
+		if err != nil {
+			return err
+		}
+		if validator := c.ResponseValidator; validator != nil {
+			err = validator(c, resp)
+			if err != nil {
+				return err
+			}
+		} else if resp.StatusCode() != 200 {
+			resp.CloseBodyStream()
+			return fmt.Errorf("could not connect to stream: %s", http.StatusText(resp.StatusCode()))
+		}
+		defer resp.CloseBodyStream()
+
+		if !connected {
+			// Notify connect
+			errch <- nil
+			connected = true
+		}
+
+		reader := NewEventStreamReader(resp.BodyStream(), c.maxBufferSize)
+		eventChan, errorChan := c.startReadLoop(reader)
+
+		for {
+			var msg *Event
+			// Wait for message to arrive or exit
+			select {
+			case <-c.subscribed[ch]:
+				return nil
+			case err = <-errorChan:
+				return err
+			case msg = <-eventChan:
+			}
+
+			// Wait for message to be sent or exit
+			if msg != nil {
+				select {
+				case <-c.subscribed[ch]:
+					return nil
+				case ch <- msg:
+					// message sent
+				}
+			}
+		}
+	}
+
+	go func() {
+		defer c.cleanup(ch)
+		// Apply user specified reconnection strategy or default to standard NewExponentialBackOff() reconnection method
+		var err error
+		if c.ReconnectStrategy != nil {
+			err = backoff.RetryNotify(operation, c.ReconnectStrategy, c.ReconnectNotify)
+		} else {
+			err = backoff.RetryNotify(operation, backoff.NewExponentialBackOff(), c.ReconnectNotify)
+		}
+
+		// channel closed once connected
+		if err != nil && !connected {
+			errch <- err
+		}
+	}()
+	err := <-errch
+	close(errch)
+	return err
+}
+
+func (c *Client) startReadLoop(reader *EventStreamReader) (chan *Event, chan error) {
+	outCh := make(chan *Event)
+	erChan := make(chan error)
+	go c.readLoop(reader, outCh, erChan)
+	return outCh, erChan
+}
+
+func (c *Client) readLoop(reader *EventStreamReader, outCh chan *Event, erChan chan error) {
+	for {
+		// Read each new line and process the type of event
+		event, err := reader.ReadEvent()
+		if err != nil {
+			if err == io.EOF {
+				erChan <- nil
+				return
+			}
+			// run user specified disconnect function
+			if c.disconnectcb != nil {
+				c.Connected = false
+				c.disconnectcb(c)
+			}
+			erChan <- err
+			return
+		}
+
+		if !c.Connected && c.connectedcb != nil {
+			c.Connected = true
+			c.connectedcb(c)
+		}
+
+		// If we get an error, ignore it.
+		var msg *Event
+		if msg, err = c.processEvent(event); err == nil {
+			if len(msg.ID) > 0 {
+				c.LastEventID.Store(msg.ID)
+			} else {
+				msg.ID, _ = c.LastEventID.Load().(string)
+			}
+
+			// Send downstream if the event has something useful
+			if msg.hasContent() {
+				outCh <- msg
+			}
+		}
+	}
+}
+
+// SubscribeRaw to an sse endpoint
+func (c *Client) SubscribeRaw(handler func(msg *Event)) error {
+	return c.Subscribe("", handler)
+}
+
+// SubscribeRawWithContext to an sse endpoint with context
+func (c *Client) SubscribeRawWithContext(ctx context.Context, handler func(msg *Event)) error {
+	return c.SubscribeWithContext(ctx, "", handler)
+}
+
+// SubscribeChanRaw sends all events to the provided channel
+func (c *Client) SubscribeChanRaw(ch chan *Event) error {
+	return c.SubscribeChan("", ch)
+}
+
+// SubscribeChanRawWithContext sends all events to the provided channel with context
+func (c *Client) SubscribeChanRawWithContext(ctx context.Context, ch chan *Event) error {
+	return c.SubscribeChanWithContext(ctx, "", ch)
+}
+
+// Unsubscribe unsubscribes a channel
+func (c *Client) Unsubscribe(ch chan *Event) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if c.subscribed[ch] != nil {
+		c.subscribed[ch] <- struct{}{}
+	}
+}
+
+// OnDisconnect specifies the function to run when the connection disconnects
+func (c *Client) OnDisconnect(fn ConnCallback) {
+	c.disconnectcb = fn
+}
+
+// OnConnect specifies the function to run when the connection is successful
+func (c *Client) OnConnect(fn ConnCallback) {
+	c.connectedcb = fn
+}
+
+func (c *Client) request(ctx context.Context, stream string) (*protocol.Response, error) {
+	req, resp := protocol.AcquireRequest(), protocol.AcquireResponse()
+	req.SetMethod("GET")
+	req.SetRequestURI(c.URL)
+
+	//req, err := adaptor.GetCompatRequest(hReq)
+	//if err != nil {
+	//	return nil, err
+	//}
+
+	//req, err := http.NewRequest("GET", c.URL, nil)
+	//if err != nil {
+	//	return nil, err
+	//}
+	//req = req.WithContext(ctx)
+
+	// Setup request, specify stream to connect to
+	if stream != "" {
+		req.URI().QueryArgs().Add("stream", stream)
+		// 不确定是否已经加入到原始请求 url 中，有待测试
+	}
+
 	req.Header.Set("Cache-Control", "no-cache")
 	req.Header.Set("Accept", "text/event-stream")
 	req.Header.Set("Connection", "keep-alive")
 
-	// 发起 SSE 连接
-	var resp *protocol.Response
-	err := c.hertzClient.Do(ctx, req, resp)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.CloseBodyStream()
-
-	if resp.StatusCode() != 200 {
-		resp.CloseBodyStream()
-		return fmt.Errorf("could not connect to stream: %s", http.StatusText(resp.StatusCode()))
+	lastID, exists := c.LastEventID.Load().([]byte)
+	if exists && lastID != nil {
+		req.Header.Set("Last-Event-ID", string(lastID))
 	}
 
-	// 读取 SSE 响应
-	go func() {
-		decoder := NewEventStreamReader(resp.Body)
+	// Add user specified headers
+	for k, v := range c.Headers {
+		req.Header.Set(k, v)
+	}
 
-		// 逐行读取 SSE 事件
-		for {
-			event, err := decoder.DecodeEvent()
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			// 处理 SSE 事件
-			fmt.Println("Received event:")
-			fmt.Println("Event:", event.Event)
-			fmt.Println("Data:", event.Data)
-			fmt.Println()
-		}
-	}()
-
-	// 等待一段时间，模拟 SSE 连接的持续性
-	time.Sleep(5 * time.Minute)
-
-	return nil
-}
-
-func NewEventStreamReader(body func() []byte) *EventStreamReader {
+	err := c.HertzClient.Do(ctx, req, resp)
+	return resp, err
 
 }
 
-func (d *EventStreamReader) DecodeEvent() (*Event, error) {
-	var event Event
+func (c *Client) processEvent(msg []byte) (event *Event, err error) {
+	var e Event
 
-	for d.scanner.Scan() {
-		line := d.scanner.Text()
+	if len(msg) < 1 {
+		return nil, errors.New("event message was empty")
+	}
 
-		// 空行表示一个事件的结束
-		if line == "" {
-			return &event, nil
-		}
-
-		// 根据 SSE 事件属性前缀进行解析
-		if strings.HasPrefix(line, "event:") {
-			event.Event = strings.TrimSpace(line[6:])
-		} else if strings.HasPrefix(line, "data:") {
-			//
-		} else if strings.HasPrefix(line, "id:") {
-			//
-		} else if strings.HasPrefix(line, "retry:") {
-			//
+	// Normalize the crlf to lf to make it easier to split the lines.
+	// Split the line by "\n" or "\r", per the spec.
+	for _, line := range bytes.FieldsFunc(msg, func(r rune) bool { return r == '\n' || r == '\r' }) {
+		switch {
+		case bytes.HasPrefix(line, headerID):
+			e.ID = string(append([]byte(nil), trimHeader(len(headerID), line)...))
+		case bytes.HasPrefix(line, headerData):
+			// The spec allows for multiple data fields per event, concatenated them with "\n".
+			e.Data = append(e.Data[:], append(trimHeader(len(headerData), line), byte('\n'))...)
+		// The spec says that a line that simply contains the string "data" should be treated as a data field with an empty body.
+		case bytes.Equal(line, bytes.TrimSuffix(headerData, []byte(":"))):
+			e.Data = append(e.Data, byte('\n'))
+		case bytes.HasPrefix(line, headerEvent):
+			e.Event = string(append([]byte(nil), trimHeader(len(headerEvent), line)...))
+		case bytes.HasPrefix(line, headerRetry):
+			// 这里采用的是大端序
+			e.Retry = binary.BigEndian.Uint64(append([]byte(nil), trimHeader(len(headerRetry), line)...))
+		default:
+			// Ignore any garbage that doesn't match what we're looking for.
 		}
 	}
 
-	if err := d.scanner.Err(); err != nil {
-		return nil, err
+	// Trim the last "\n" per the spec.
+	e.Data = bytes.TrimSuffix(e.Data, []byte("\n"))
+
+	if c.EncodingBase64 {
+		buf := make([]byte, base64.StdEncoding.DecodedLen(len(e.Data)))
+
+		n, err := base64.StdEncoding.Decode(buf, e.Data)
+		if err != nil {
+			err = fmt.Errorf("failed to decode event message: %s", err)
+		}
+		e.Data = buf[:n]
+	}
+	return &e, err
+}
+
+func (c *Client) cleanup(ch chan *Event) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if c.subscribed[ch] != nil {
+		close(c.subscribed[ch])
+		delete(c.subscribed, ch)
+	}
+}
+
+func trimHeader(size int, data []byte) []byte {
+	if data == nil || len(data) < size {
+		return data
 	}
 
-	return nil, errors.New("unexpected end of SSE stream")
+	data = data[size:]
+	// Remove optional leading whitespace
+	if len(data) > 0 && data[0] == 32 {
+		data = data[1:]
+	}
+	// Remove trailing new line
+	if len(data) > 0 && data[len(data)-1] == 10 {
+		data = data[:len(data)-1]
+	}
+	return data
 }
